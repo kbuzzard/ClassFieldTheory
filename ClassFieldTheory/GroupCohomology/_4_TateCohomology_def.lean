0e529d8234f5954b93157137d656e632d1b9b30f
Whysoserioushah <yx3021@ic.ac.uk>
2025-07-21 17:47:31 +0100

    for now

--- ClassFieldTheory/GroupCohomology/_4_TateCohomology_def.lean
+++ ClassFieldTheory/GroupCohomology/_4_TateCohomology_def.lean
@@ -55,6 +55,7 @@ This is the map from the coinvariants of `M : Rep R G` to the invariants, induce
 def TateNorm (M : Rep R G) : (inhomogeneousChains M).X 0 ⟶ (inhomogeneousCochains M).X 0 :=
   (chainsIso₀ M).hom ≫ M.norm ≫ (cochainsIso₀ M).inv
 
+omit [DecidableEq G] in
 lemma TateNorm.def (M : Rep R G) :
     TateNorm M = (chainsIso₀ M).hom ≫ M.norm ≫ (cochainsIso₀ M).inv := rfl
 
@@ -70,6 +71,7 @@ lemma norm_comp_d_eq_zero (M : Rep R G) : M.norm ≫ (d₀₁ M) = 0 := by
   intro g
   rw [← LinearMap.comp_apply, Representation.norm_comm]
 
+omit [DecidableEq G] in
 lemma TateNorm_comp_d (M : Rep R G) : TateNorm M ≫ (inhomogeneousCochains M).d 0 1 = 0 := by
   simp only [ChainComplex.of_x, CochainComplex.of_x, TateNorm.def, Category.assoc, eq_d₀₁_comp_inv,
     Preadditive.IsIso.comp_left_eq_zero]
@@ -87,6 +89,7 @@ lemma comp_eq_zero' (M : Rep R G) : (groupHomology.d₁₀ M) ≫ M.norm = 0 := 
   rw [← LinearMap.comp_apply, Representation.norm_comm']
   simp
 
+omit [DecidableEq G] in
 lemma d_comp_TateNorm (M : Rep R G) : (inhomogeneousChains M).d 1 0 ≫ TateNorm M = 0 := by
   simp only [ChainComplex.of_x, CochainComplex.of_x, ← Category.assoc,
     Preadditive.IsIso.comp_right_eq_zero]
@@ -101,11 +104,14 @@ def TateComplex.ConnectData (M : Rep R G) :
 def TateComplex (M : Rep R G) : CochainComplex (ModuleCat R) ℤ :=
   CochainComplex.ConnectData.cochainComplex (TateComplex.ConnectData M)
 
+omit [DecidableEq G] in
 lemma TateComplex_d_neg_one (M : Rep R G) : (TateComplex M).d (-1) 0 = TateNorm M := rfl
 
+omit [DecidableEq G] in
 lemma TateComplex_d_ofNat (M : Rep R G) (n : ℕ) :
     (TateComplex M).d n (n + 1) = (inhomogeneousCochains M).d n (n + 1) := rfl
 
+omit [DecidableEq G] in
 lemma TateComplex_d_neg (M : Rep R G) (n : ℕ) :
     (TateComplex M).d (-(n + 2 : ℤ)) (-(n + 1 : ℤ)) = (inhomogeneousChains M).d (n + 1) n := rfl
 
@@ -162,13 +168,14 @@ def TateComplexFunctor : Rep R G ⥤ CochainComplex (ModuleCat R) ℤ where
         subst ha
         simp [TateComplex, TateComplex.ConnectData]
         ext : 1
-        simp only [ModuleCat.hom_comp, chainsMap_id_f_hom_eq_mapRange,
+        simp only [ModuleCat.hom_comp,
           cochainsMap_id_f_hom_eq_compLeft]
         apply LinearMap.ext
         intro v
         ext i
-        simp [cochainsIso₀, chainsIso₀]
-        rw [← LinearMap.comp_apply, ← ModuleCat.hom_comp, TateComplex.norm_comm]
+        simp [cochainsIso₀]
+        rw [← LinearMap.comp_apply, ← ModuleCat.hom_comp]
+        erw [TateComplex.norm_comm]
         simp
         else
         simp
@@ -178,17 +185,18 @@ def TateComplexFunctor : Rep R G ⥤ CochainComplex (ModuleCat R) ℤ where
           rw [add_comm]; ring
         rw [this]
         simp [TateComplex]}
-  map_id := sorry
-  map_comp := sorry
+  map_comp := by sorry
 
 def TateCohomology (n : ℤ) : Rep R G ⥤ ModuleCat R :=
   TateComplexFunctor ⋙ HomologicalComplex.homologyFunctor _ _ n
 
+-- #synth groupCohomology
 /-
 The next two statements say that `TateComplexFunctor` is an exact functor.
 -/
 instance TateComplexFunctor_preservesFiniteLimits :
-    PreservesFiniteLimits (TateComplexFunctor (R := R) (G := G)) :=
+    PreservesFiniteLimits (TateComplexFunctor (R := R) (G := G)) := by
+
   sorry
 
 instance TateComplexFunctor_preservesFiniteColimits :
@@ -226,10 +234,16 @@ def TateCohomology.iso_groupCohomology (n : ℕ)  :
   -- -- convert Iso.refl _
   -- -- sorry
 
--- def TateCohomology.iso_groupHomology (n : ℕ) (M : Rep R G) :
---     (TateCohomology (-n - 2)) ≅ groupHomology.functor M (n + 1) := by
---   convert Iso.refl _
---   sorry
+def TateCohomology.iso_groupHomology (n : ℕ) :
+    (TateCohomology (-n - 2)) ≅ groupHomology.functor R G (n + 1) :=
+  NatIso.ofComponents (fun M ↦ CochainComplex.ConnectData.homologyIsoNeg
+    (TateComplex.ConnectData M) _ _ (by norm_num; rw [add_comm]; rfl)) <| fun {X Y} f ↦ by
+    ext m
+    simp
+
+
+
+    sorry
 
 -- def TateCohomology.isoGroupCohomology (n : ℕ) (M : Rep R G) :
 --     (TateCohomology (n + 1)).obj M ≅ groupCohomology M (n + 1) := (isoGroupCohomology' n).app M
