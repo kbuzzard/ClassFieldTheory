84fa2415063dfd6c694b8d6182293403b5eb1984
Justus Springer <justusspringer@gmx.de>
2025-07-24 19:24:02 +0200

    use sorried normal basis theorem

--- ClassFieldTheory/GroupCohomology/GaloisExtensionInd.lean
+++ ClassFieldTheory/GroupCohomology/GaloisExtensionInd.lean
@@ -1,5 +1,19 @@
 import ClassFieldTheory.GroupCohomology._7_coind1_and_ind1
-import ClassFieldTheory.GroupCohomology.NormalBasisTheorem
+
+-- Sorried version of normal basis theorem, see mathlib PR #27390
+namespace IsGalois
+
+variable (K L : Type*) [Field K] [Field L] [Algebra K L] [FiniteDimensional K L] [IsGalois K L]
+
+/-- Given a finite Galois extension `L/K`, `normalBasis K L` is a basis of `L` over `K`
+that is an orbit under the Galois group action. -/
+noncomputable def normalBasis : Basis (L ≃ₐ[K] L) K L := sorry
+
+variable {K L}
+
+theorem normalBasis_apply (e : L ≃ₐ[K] L) : normalBasis K L e = e (normalBasis K L 1) := sorry
+
+end IsGalois
 
 namespace AlgEquiv
 variable {R A : Type*} [CommSemiring R] [Semiring A] [Algebra R A]
