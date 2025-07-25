688aa9c36f4562bf7d33339552ed132c678e3cbc
Yaël Dillies <yael.dillies@gmail.com>
2025-07-25 15:31:16 +0000

    chore: prefix file numbers by `0`s This makes sure that `lake exe mk_all` doesn't mess with the order of `ClassFieldTheory.lean` (too much).

--- ClassFieldTheory/GroupCohomology/_13_HerbrandQuotient.lean
+++ ClassFieldTheory/GroupCohomology/_13_HerbrandQuotient.lean
@@ -1,6 +1,6 @@
 import Mathlib
-import ClassFieldTheory.GroupCohomology._9_CyclicGroup
-import ClassFieldTheory.GroupCohomology._4_TateCohomology_def
+import ClassFieldTheory.GroupCohomology._09_CyclicGroup
+import ClassFieldTheory.GroupCohomology._04_TateCohomology_def
 
 noncomputable section
 
