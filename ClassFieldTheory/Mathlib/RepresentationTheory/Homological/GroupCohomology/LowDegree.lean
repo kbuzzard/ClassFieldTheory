b586ab3821e49cba4bcbc63a994bf7f1790a37ce
Yaël Dillies <yael.dillies@gmail.com>
2025-08-03 18:22:44 +0000

    more

--- ClassFieldTheory/Mathlib/RepresentationTheory/Homological/GroupCohomology/LowDegree.lean
+++ ClassFieldTheory/Mathlib/RepresentationTheory/Homological/GroupCohomology/LowDegree.lean
@@ -1,3 +1,5 @@
+import ClassFieldTheory.Mathlib.Algebra.Category.ModuleCat.Basic
+import ClassFieldTheory.Mathlib.GroupTheory.Torsion
 import Mathlib.RepresentationTheory.Homological.GroupCohomology.LowDegree
 
 open CategoryTheory Limits
@@ -10,23 +12,16 @@ variable {R G : Type u} [CommRing R] [Group G] {A : Rep R G}
 If `M` is a trivial representation of a finite group `G` and `M` is torsion-free
 then `H¹(G,M) = 0`.
 -/
-lemma H1_isZero_of_trivial (M : Rep R G) [NoZeroSMulDivisors ℕ M] [M.IsTrivial] [Finite G] :
+lemma H1_isZero_of_trivial (M : Rep R G) [IsAddTorsionFree M] [M.IsTrivial] [Finite G] :
     IsZero (H1 M) := by
   /-
-  Since `M` is a trivial representation, we can identify `H¹(G,M)` with `Hom(G,M)`,
+  Since `M` is a trivial representation, we can identify `H¹(G, M)` with `Hom(G, M)`,
   which is zero if `G` is finite and `M` is torsion-free.
 
-  This uses `groupCohomology.H1LequivOfIsTrivial`.
+  This uses `groupCohomology.H1IsoOfIsTrivial`.
   -/
   refine .of_iso ?_ (groupCohomology.H1IsoOfIsTrivial M)
-  have (f : (ModuleCat.of R (Additive G →+ ↑M.V))) : f = 0 := by
-    ext g
-    have : IsOfFinAddOrder (Additive.ofMul g) := isOfFinAddOrder_of_finite _
-    obtain ⟨n, hpos, hn⟩ := this.exists_nsmul_eq_zero
-    have : n • f (Additive.ofMul g) = 0 := by simp [← AddMonoidHom.map_nsmul, hn]
-    aesop
-  have : Subsingleton (ModuleCat.of R (Additive G →+ ↑M.V)) := subsingleton_of_forall_eq 0 this
-  exact ModuleCat.isZero_of_subsingleton (ModuleCat.of R (Additive G →+ ↑M.V))
+  simp [subsingleton_addMonoidHom_of_isTorsion_isAddTorsionFree, isTorsion_of_finite]
 
 namespace cocycles₂
 
