5c714b2f8900192a02b567545c1dae18b7bd9ce4
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-10-13 19:15:26 +0100

    obsolete from mk_all

--- ClassFieldTheory.lean
+++ ClassFieldTheory.lean
@@ -2,11 +2,6 @@ import ClassFieldTheory.GroupCohomology.Examples.Examples
 import ClassFieldTheory.GroupCohomology.HerbrandInt
 import ClassFieldTheory.GroupCohomology.IndCoind.TrivialCohomology
 import ClassFieldTheory.GroupCohomology.LocalInv
-import ClassFieldTheory.GroupCohomology.obsolete.CyclicGroup
-import ClassFieldTheory.GroupCohomology.obsolete.HerbrandQuotient_old
-import ClassFieldTheory.GroupCohomology.obsolete._0_Current_PRs
-import ClassFieldTheory.GroupCohomology.obsolete._4_DimensionShift
-import ClassFieldTheory.GroupCohomology.obsolete.groupCohomology
 import ClassFieldTheory.GroupCohomology.«02_restriction»
 import ClassFieldTheory.GroupCohomology.«03_inflation»
 import ClassFieldTheory.GroupCohomology.«04_TateCohomology_def»
