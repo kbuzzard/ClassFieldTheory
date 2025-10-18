1590cbcfc8609679f303c3b13eef690253ecbec4
Whysoserioushah <yx3021@ic.ac.uk>
2025-10-18 14:34:52 +0100

    sorry-free file 12

--- ClassFieldTheory/GroupCohomology/12_augmentationModule.lean
+++ ClassFieldTheory/GroupCohomology/12_augmentationModule.lean
@@ -225,9 +225,21 @@ abbrev leftRegular.norm' [Fintype G] : (leftRegular R G).ρ.invariants :=
     simpa [leftRegular.of] using show ∑ x : G, leftRegular.of (g * x) = _ from
     Finset.sum_equiv (Equiv.mulLeft g) (by grind) <| fun _ _ ↦ rfl⟩
 
+abbrev leftRegular.res_norm' [Fintype G] {H : Type} [Group H] (g : G) (φ : H →* G)
+    (inj : φ.toFun.Injective) : (leftRegular R G ↓ φ).ρ.invariants :=
+  letI : Fintype H := Fintype.ofInjective _ inj
+  ⟨∑ h : H, leftRegular.of (φ h * g), fun h ↦ by
+    simpa [leftRegular.of] using show ∑ x : H, leftRegular.of _ = _ by
+      simpa [← mul_assoc, ← leftRegular.of_def] using Finset.sum_equiv (Equiv.mulLeft h) (by grind)
+        (by simp)⟩
+
 attribute [local simp] leftRegular.of in
 def leftRegular.norm [Fintype G] : H0 (leftRegular R G) :=
   (H0Iso (leftRegular R G)).toLinearEquiv.symm (leftRegular.norm' R G)
+
+def leftRegular.res_norm [Fintype G] {H : Type} [Group H] (φ : H →* G) (inj : φ.toFun.Injective) (g : G) :
+    H0 ((leftRegular R G) ↓ φ) :=
+  (H0Iso ((leftRegular R G) ↓ φ)).toLinearEquiv.symm (leftRegular.res_norm' R G g φ inj)
 
 open RepresentationTheory.groupCohomology
 
@@ -240,6 +252,19 @@ lemma leftRegular.zeroι_norm [Fintype G] :
       Finset.sum_equiv (Equiv.mulLeft g) (by grind) <| fun _ _ ↦ rfl⟩
   exact congr($this)
 
+lemma leftRegular.H0Iso_res_norm [Fintype G] {H : Type} [Group H] (φ : H →* G)
+    (inj : φ.toFun.Injective) (g : G) :
+    (H0Iso ((leftRegular R G) ↓ φ)).hom (res_norm R G φ inj g) = res_norm' R G g φ inj :=
+  (H0Iso _).toLinearEquiv.apply_symm_apply _
+
+lemma leftRegular.zeroι_res_norm [Fintype G] {H : Type} [Group H] (φ : H →* G)
+    (inj : φ.toFun.Injective) (g : G) :
+    letI : Fintype H := Fintype.ofInjective _ inj
+    zeroι _ (res_norm R G φ inj g) = ∑ h : H, leftRegular.of (φ h * g) := by
+  letI : Fintype H := Fintype.ofInjective _ inj
+  dsimp [zeroι]
+  erw [leftRegular.H0Iso_res_norm] -- `erw?` does nothing
+
 lemma leftRegular.span_norm' [Fintype G] :
     Submodule.span R {norm' R G} = ⊤ := by
   ext ⟨x, hx⟩
@@ -249,13 +274,88 @@ lemma leftRegular.span_norm' [Fintype G] :
     simpa using Finsupp.ext_iff.1 (hx g⁻¹) 1⟩
   exact ⟨hx.choose, Finsupp.ext_iff.2 fun g ↦ by simp [← hx.choose_spec g, leftRegular.of]⟩
 
+-- instance [Fintype G] {H} [Group H] (φ : H →* G) (inj : Function.Injective φ) :
+--   Fintype {(leftRegular.res_norm' R G g φ inj) | g : G} := by exact?
+-- attribute [local simp] Fintype.ofFinite in
+lemma leftRegular.res_span_norm' [Fintype G] {H : Type} [Group H] (φ : H →* G) (inj : φ.toFun.Injective) :
+    Submodule.span R {res_norm' R G g φ inj | g : G} = ⊤ := by
+  -- classical
+  apply le_antisymm
+  · simp
+  · rintro x -
+    choose σ hσ using Quotient.mk_surjective (s := QuotientGroup.rightRel φ.range)
+    classical
+
+    have : x = ∑ i, (show G →₀ _ from x.1) (σ i) • res_norm' R G (σ i) φ inj := by
+      ext;
+      simp [Finsupp.ext_iff, leftRegular.of_apply (R := R)]
+      intro a
+      rw [Finset.sum_eq_single (Quotient.mk _ a)]
+      · have (i j : G) : QuotientGroup.rightRel φ.range i j → (show G →₀ R from x.1) i =
+          (show G →₀ R from x.1) j := fun hij ↦ by
+          obtain ⟨x, hx⟩ := x
+          simp
+          simp at hx
+          obtain ⟨k, hk⟩ := by simpa [QuotientGroup.rightRel_apply] using hij
+          have := by simpa [Representation.ofMulAction_def, Finsupp.ext_iff] using hx k
+          simpa [mapDomain, Finsupp.sum_fintype, Finsupp.single_apply,
+            hk, mul_assoc, inv_mul_eq_one] using this j
+        simp at this
+        rw [this a (σ ⟦a⟧) (by rw [← Quotient.eq, hσ])]
+        letI := Fintype.ofInjective ⇑φ inj
+        suffices @Finset.card H {x | φ x * σ ⟦a⟧ = a} = 1 by simp [this]
+        rw [Finset.card_eq_one]
+        have := hσ ⟦a⟧
+        rw [Quotient.eq] at this
+        obtain ⟨⟨_, ⟨h, rfl⟩⟩, hhh⟩ := this
+        use h⁻¹
+        ext h'
+        simp
+        constructor
+        · intro final
+          simp [← hhh] at final
+          change _ * (_ * _) = _ at final
+          simp [← mul_assoc] at final
+          rw [← φ.map_mul, ← φ.map_one] at final
+          apply inj at final
+          exact eq_inv_of_mul_eq_one_left final
+        rintro rfl
+        simp [← hhh]
+        change _ * (_ * _) = _
+        simp
+      · intro b _ hab
+        have : Fintype H := Fintype.ofInjective _ inj
+        suffices Finset.card {x | φ x * σ b = a} = 0 by simp_all
+        simp only [Finset.card_eq_zero, Finset.filter_eq_empty_iff, Finset.mem_univ, forall_const]
+        intro h eq
+        apply_fun Quotient.mk (QuotientGroup.rightRel φ.range) at eq
+        refine hab ?_
+        nth_rw 1 [← hσ b]
+        simp [← eq, QuotientGroup.rightRel_apply]
+      · simp
+    rw [this]
+    apply sum_mem
+    intro i _
+    apply Submodule.smul_mem
+    exact Submodule.subset_span ⟨σ i, rfl⟩
+
+
 lemma leftRegular.span_norm [Fintype G] : Submodule.span R {norm R G} = ⊤ := by
   rw [norm, ← Set.image_singleton, ← Submodule.map_span, leftRegular.span_norm']
   simp
 
+lemma leftRegular.res_span_norm [Fintype G] {H : Type} [Group H] (φ : H →* G)
+    (inj : φ.toFun.Injective) :
+    letI : Fintype H := Fintype.ofInjective _ inj
+    Submodule.span R (Set.range (res_norm R G φ inj)) = ⊤ := by
+
+  sorry
+
 def H0trivial : H0 (trivial R G R) ≅ ModuleCat.of R R :=
   LinearEquiv.toModuleIso <| LinearEquiv.symm <| (Submodule.topEquiv.symm ≪≫ₗ LinearEquiv.ofEq _ _
   (by ext; simp) ≪≫ₗ (CategoryTheory.Iso.toLinearEquiv (H0Iso (trivial R G R))).symm)
+
+
 
 @[elementwise]
 lemma blah2 {ρ : Rep R G} (f : ρ ⟶ trivial R G R) :
@@ -268,44 +368,54 @@ lemma blah2 {ρ : Rep R G} (f : ρ ⟶ trivial R G R) :
   rw [ModuleCat.Iso_hom, ← LinearMap.comp_apply, ← ModuleCat.hom_comp, map_id_comp_H0Iso_hom]
   simp [zeroι]
 
+lemma leftRegular.groupCoh_map_res_norm [Fintype G] {H : Type} [Group H] (φ : H →* G)
+    (inj : φ.toFun.Injective) (g : G) :
+    letI : Fintype H := Fintype.ofInjective _ inj
+    groupCohomology.map (.id _) ((res φ).map (ε R G)) 0 (res_norm R G φ inj g) =
+      (H0trivial R H).toLinearEquiv.symm (Fintype.card H : R) := by
+  apply (H0trivial R H).toLinearEquiv.eq_symm_apply.mpr
+  erw [blah2_apply]
+  rw [leftRegular.zeroι_res_norm]
+  simp [ε, leftRegular.of]
+
 def H1_iso [Fintype G] :
     H1 (aug R G) ≅ ModuleCat.of R (R ⧸ Ideal.span {(Nat.card G : R)}) :=
   LinearEquiv.toModuleIso <| LinearEquiv.symm <| by
   refine ?_ ≪≫ₗ LinearMap.quotKerEquivOfSurjective (δ (aug_isShortExact R G) 0 1 rfl).hom
     (ModuleCat.epi_iff_surjective _|>.1 <| epi_δ_of_isZero _ 0 <| by
-    simpa using isZero_of_trivialCohomology)
-  · refine Submodule.Quotient.equiv _ _ (H0trivial R G).symm.toLinearEquiv ?_
-    erw [← CategoryTheory.ShortComplex.Exact.moduleCat_range_eq_ker <|
-      (mapShortComplex₃_exact) (aug_isShortExact R G) (rfl : 0 + 1 = 1)]
-    apply le_antisymm
-    · simp only [Nat.card_eq_fintype_card, Nat.reduceAdd]
-      intro (x : H0 _)
-      simp only [Submodule.mem_map, Ideal.mem_span_singleton', exists_exists_eq_and,
-        LinearMap.mem_range, forall_exists_index]
-      rintro a rfl
-      refine ⟨a • leftRegular.norm R G, ?_⟩
-      apply (H0trivial R G).toLinearEquiv.injective
-      erw [ModuleCat.Iso_symm_iso, LinearEquiv.apply_symm_apply, blah2_apply]
-      simp only [map_smul, smul_eq_mul]
-      rw [leftRegular.zeroι_norm, map_sum]
-      conv => enter [1, 2, 2, x]; erw [ε_of]
-      simp
-    · erw [Submodule.map_equiv_eq_comap_symm]
-      rw [LinearMap.range_eq_map, ← leftRegular.span_norm, Submodule.map_le_iff_le_comap,
-        Submodule.span_le, Set.singleton_subset_iff]
-      simp only [Nat.reduceAdd, ShortComplex.SnakeInput.L₁'_X₁,
-        HomologicalComplex.HomologySequence.snakeInput_L₀, Functor.mapShortComplex_obj,
-        ShortComplex.map_X₂, cochainsFunctor_obj, HomologicalComplex.homologyFunctor_obj,
-        ShortComplex.SnakeInput.L₁'_X₂, ShortComplex.map_X₃, ShortComplex.SnakeInput.L₁'_f,
-        ShortComplex.map_g, cochainsFunctor_map, HomologicalComplex.homologyFunctor_map,
-        Nat.card_eq_fintype_card, Submodule.comap_coe, LinearEquiv.coe_coe, Set.mem_preimage,
-        SetLike.mem_coe]
-      erw [blah2_apply]
-      erw [leftRegular.zeroι_norm]
-      convert_to (hom (ε R G).hom) (∑ g, leftRegular.of g) ∈ Ideal.span {(Fintype.card G : R)}
-      rw [map_sum]
-      conv => enter [2, 2, x]; tactic => erw [ε_of]
-      simpa [Ideal.mem_span_singleton'] using ⟨1, one_mul _⟩
+    simpa using by exact isZero_of_trivialCohomology)
+  refine Submodule.Quotient.equiv _ _ (H0trivial R G).symm.toLinearEquiv ?_
+  erw [← CategoryTheory.ShortComplex.Exact.moduleCat_range_eq_ker <|
+    (mapShortComplex₃_exact) (aug_isShortExact R G) (rfl : 0 + 1 = 1)]
+  apply le_antisymm
+  · simp only [Nat.card_eq_fintype_card, Nat.reduceAdd]
+    intro (x : H0 _)
+    simp only [Submodule.mem_map, Ideal.mem_span_singleton', exists_exists_eq_and,
+      LinearMap.mem_range, forall_exists_index]
+    rintro a rfl
+    refine ⟨a • leftRegular.norm R G, ?_⟩
+    apply (H0trivial R G).toLinearEquiv.injective
+    erw [ModuleCat.Iso_symm_iso, LinearEquiv.apply_symm_apply, blah2_apply]
+    simp only [map_smul, smul_eq_mul]
+    rw [leftRegular.zeroι_norm, map_sum]
+    conv => enter [1, 2, 2, x]; erw [ε_of]
+    simp
+  · erw [Submodule.map_equiv_eq_comap_symm]
+    rw [LinearMap.range_eq_map, ← leftRegular.span_norm, Submodule.map_le_iff_le_comap,
+      Submodule.span_le, Set.singleton_subset_iff]
+    simp only [Nat.reduceAdd, ShortComplex.SnakeInput.L₁'_X₁,
+      HomologicalComplex.HomologySequence.snakeInput_L₀, Functor.mapShortComplex_obj,
+      ShortComplex.map_X₂, cochainsFunctor_obj, HomologicalComplex.homologyFunctor_obj,
+      ShortComplex.SnakeInput.L₁'_X₂, ShortComplex.map_X₃, ShortComplex.SnakeInput.L₁'_f,
+      ShortComplex.map_g, cochainsFunctor_map, HomologicalComplex.homologyFunctor_map,
+      Nat.card_eq_fintype_card, Submodule.comap_coe, LinearEquiv.coe_coe, Set.mem_preimage,
+      SetLike.mem_coe]
+    erw [blah2_apply]
+    erw [leftRegular.zeroι_norm]
+    convert_to (hom (ε R G).hom) (∑ g, leftRegular.of g) ∈ Ideal.span {(Fintype.card G : R)}
+    rw [map_sum]
+    conv => enter [2, 2, x]; tactic => erw [ε_of]
+    simpa [Ideal.mem_span_singleton'] using ⟨1, one_mul _⟩
 
   /-
   If Tate cohomology is defined, then this is proved in the same way as the previous
@@ -320,10 +430,36 @@ def H1_iso [Fintype G] :
   since every element of the group is mapped by `ε` to `1`.
   -/
 
-
-def H1_iso' [Finite G] {H : Type} [Group H] [DecidableEq H] {φ : H →* G}
+def H1_iso' [Fintype G] {H : Type} [Group H] [DecidableEq H] {φ : H →* G}
     (inj : Function.Injective φ) :
     H1 (aug R G ↓ φ) ≅ ModuleCat.of R (R ⧸ Ideal.span {(Nat.card H : R)}) :=
+  LinearEquiv.toModuleIso <| LinearEquiv.symm <| by
+  letI : Fintype H := Fintype.ofInjective (⇑φ) inj
+  have : ShortComplex.Exact {
+    X₁ := H0 (leftRegular R G ↓ φ)
+    X₂ := H0 (trivial R H R)
+    X₃ := H1 (aug R G ↓ φ)
+    f := groupCohomology.map (.id _) ((res φ).map (ε R G)) 0
+    g := groupCohomology.δ (aug_isShortExact' R G φ) 0 1 rfl
+    zero := _
+  } := mapShortComplex₃_exact (aug_isShortExact' R G φ) (rfl : 0 + 1 = 1)
+  refine ?_ ≪≫ₗ LinearMap.quotKerEquivOfSurjective (δ (aug_isShortExact' R G φ) 0 1 rfl).hom
+    (ModuleCat.epi_iff_surjective _|>.1 <| epi_δ_of_isZero _ 0 <| by
+    simpa using @isZero_of_trivialCohomology R H _ _ _
+      (Rep.trivialCohomology_iff_res.1 (trivialCohomology R G) φ inj) ..)
+  refine Submodule.Quotient.equiv _ _ (H0trivial R H).symm.toLinearEquiv ?_
+  erw [← CategoryTheory.ShortComplex.Exact.moduleCat_range_eq_ker this]
+  simp only [ShortComplex.map_X₃]
+  rw [LinearMap.range_eq_map, ← leftRegular.res_span_norm R G φ inj, Submodule.map_span,
+    ← Set.range_comp]
+  erw [Submodule.map_span]
+  congr 1
+  ext
+  simp
+  conv => enter [2, 1, y]; erw [leftRegular.groupCoh_map_res_norm]
+  simp [eq_comm]
+  rfl
+
   /-
   If Tate cohomology is defined, then this is proved in the same way as the previous
   lemma. If not, then using usual cohomology we have a long exact sequence containing the
@@ -336,7 +472,6 @@ def H1_iso' [Finite G] {H : Type} [Group H] [DecidableEq H] {φ : H →* G}
   The image of such a function in H⁰(H,R) is |H|, since every element of the
   group is mapped by `ε` to `1`.
   -/
-  sorry
 
 end Rep.aug
 
