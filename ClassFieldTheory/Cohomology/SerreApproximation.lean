b20e9f82fdfe6acd23850bfefcdc7ddf67cd4d31
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-11-05 21:53:17 +0000

    more progress

--- ClassFieldTheory/Cohomology/SerreApproximation.lean
+++ ClassFieldTheory/Cohomology/SerreApproximation.lean
@@ -41,9 +41,16 @@ instance subrepToSubmodule {k G : Type u} [CommRing k] [Monoid G] (M : Rep k G)
     IsFilterComplete fun i ↦ (M_ i).toSubmodule :=
   sorry
 
-instance piLeft {k M : Type u} [CommRing k] [AddCommGroup M] [Module k M]
-    {ι : Type*} [LE ι] (M_ : ι → Submodule k M) [IsFilterComplete M_] (α : Type u) :
+instance piLeft {k M ι : Type*} [Ring k] [AddCommGroup M] [Module k M]
+    [LE ι] (M_ : ι → Submodule k M) [IsFilterComplete M_] (α : Type*) :
     IsFilterComplete fun i ↦ Submodule.pi .univ fun _ : α ↦ M_ i :=
+  sorry
+
+theorem ker {R M N ι : Type*} [Ring R] [AddCommGroup M] [Module R M]
+    [AddCommGroup N] [Module R N] [LE ι] (F : ι → Submodule R M) (G : ι → Submodule R N)
+    [IsFilterComplete F] [IsFilterComplete G]
+    (φ : M →ₗ[R] N) (hφ : ∀ i x, x ∈ F i → φ x ∈ G i) :
+    IsFilterComplete fun i ↦ (F i).submoduleOf (LinearMap.ker φ) :=
   sorry
 
 end IsFilterComplete
@@ -63,18 +70,96 @@ theorem map_inclusion_surjective_of_subsingleton_groupCohomology_subquotient (q 
   rw [← ModuleCat.epi_iff_surjective]
   exact (mapShortComplex₂_exact (Subrep.shortExact_of_le h) q).epi_f <| IsZero.eq_of_tgt h₀ ..
 
+noncomputable def subrepToSubmodule (w : Subrep M) (q : ℕ) :
+    Submodule k ((inhomogeneousCochains M).X q) :=
+  Submodule.pi .univ fun _ ↦ w.toSubmodule
+
+theorem subrepToSubmodule_le_comap_d (w : Subrep M) (q : ℕ) :
+    subrepToSubmodule w q ≤
+    (subrepToSubmodule w (q + 1)).comap ((inhomogeneousCochains M).d q (q + 1)).hom := by
+  intro x hx f _
+  simp only [CochainComplex.of_x, inhomogeneousCochains.d_def, inhomogeneousCochains.d_hom_apply,
+    SetLike.mem_coe]
+  exact add_mem (w.2 (f 0) (hx _ trivial)) <| sum_mem fun _ _ ↦
+    SMulMemClass.smul_mem _ <| hx _ trivial
+
+instance (q : ℕ) : IsFilterComplete fun i ↦ subrepToSubmodule (M_ i) q :=
+  inferInstanceAs <| IsFilterComplete fun _ ↦ Submodule.pi _ _
+
+/-- `C(M_i)` is a submodule of `C(M)`. -/
+@[simps!] def subrepToSubmoduleIso (w : Subrep M) (q : ℕ) :
+    subrepToSubmodule w q ≃ₗ[k] (inhomogeneousCochains w.toRep).X q where
+  toFun x := fun f ↦ ⟨_, x.2 f trivial⟩
+  invFun x := ⟨_, fun f _ ↦ (x f).2⟩
+  map_add' _ _ := rfl
+  map_smul' _ _ := rfl
+
+/-- `d : C(M_i) ⟶ C(M_i)` commutes with `d : C(M) ⟶ C(M)`. -/
+theorem d_commutes {w : Subrep M} {q : ℕ} {x : subrepToSubmodule w q} (f) :
+    (inhomogeneousCochains.d w.toRep q (subrepToSubmoduleIso w q x) f).val =
+    inhomogeneousCochains.d M q x.val f := by
+  simp [inhomogeneousCochains.d_hom_apply, subrepToSubmoduleIso, Subrep.toRep]
+
+-- move
+@[ext] theorem _root_.Subrep.toRep_ext {k G : Type u} [CommRing k] [Monoid G] {A : Rep k G}
+    {w : Subrep A} {x y : w.toRep.V} (h : x.val = y.val) : x = y :=
+  Subtype.ext h
+
+-- move
+@[simp] theorem _root_.groupCohomology.inhomogeneousCochains.d_d
+    {k G : Type u} [CommRing k] {n : ℕ} [Group G] {A : Rep k G}
+    (x : (inhomogeneousCochains A).X n) :
+    inhomogeneousCochains.d A (n + 1) (inhomogeneousCochains.d A n x) = 0 :=
+  congr($(inhomogeneousCochains.d_comp_d n A) x)
+
+@[simp] theorem _root_.groupCohomology.inhomogeneousCochains.d_d_apply
+    {k G : Type u} [CommRing k] {n : ℕ} [Group G] {A : Rep k G}
+    (x : (inhomogeneousCochains A).X n) (f) :
+    inhomogeneousCochains.d A (n + 1) (inhomogeneousCochains.d A n x) f = 0 :=
+  congr($(inhomogeneousCochains.d_d x) f)
+
+-- /-- `Z(M_i)` is a submodule of `Z(M)`. -/
+
 include hm hm0 in
 /-
 Needed facts:
-1. H^{q+1}(M_{i+1}) → H^{q+1}(M_i) is surjective
+1. (✓) H^{q+1}(M_{i+1}) → H^{q+1}(M_i) is surjective
 2. C^q(M_i) ⊆ C^q(M) is precomplete
-3. Z^{q+1}(M_i) ⊆ Z^q(M) is Hausdorff
+3. Z^{q+1}(M_i) ⊆ Z^{q+1}(M) is Hausdorff
 -/
 theorem subsingleton_of_subquotient (q : ℕ)
-    [h : ∀ i, Subsingleton (groupCohomology ((M_ i).subquotient (M_ (i + 1))) (q + 1))] :
+    [h₀ : ∀ i, Subsingleton (groupCohomology ((M_ i).subquotient (M_ (i + 1))) (q + 1))] :
     Subsingleton (groupCohomology M (q + 1)) := by
-  simp_rw [← ModuleCat.isZero_iff_subsingleton, groupCohomology, ← exactAt_iff_isZero_homology,
-    exactAt_iff, moduleCat_exact_iff]
+  rw [← ModuleCat.isZero_iff_subsingleton, groupCohomology, ← exactAt_iff_isZero_homology,
+    exactAt_iff' _ q (q + 1) (q + 2) (by simp) (by simp), moduleCat_exact_iff]
+  have h₁ (i) := map_inclusion_surjective_of_subsingleton_groupCohomology_subquotient
+    (q + 1) (hm (Nat.le_succ i))
+  have h₂ : IsFilterComplete fun i ↦ subrepToSubmodule (M_ i) q := inferInstance
+  have h₃ := IsFilterComplete.ker
+    (fun i ↦ subrepToSubmodule (M_ i) (q + 1))
+    (fun i ↦ subrepToSubmodule (M_ i) (q + 2))
+    ((inhomogeneousCochains M).d (q + 1) (q + 2)).hom
+    fun _ ↦ subrepToSubmodule_le_comap_d _ _
+  intro (x : (inhomogeneousCochains M).X (q + 1)) hx
+  simp only [shortComplexFunctor'_obj_X₃, CochainComplex.of_x, shortComplexFunctor'_obj_X₂,
+    shortComplexFunctor'_obj_g, inhomogeneousCochains.d_def] at hx
+  -- hx : inhomogeneousCochains.d M (q + 1) x = 0
+  -- having chosen the first `n` elements, we can choose the `n+1`-st
+  have h₄ (n : ℕ) (ih : Fin n → (inhomogeneousCochains M).X q)
+      (h : x - ∑ i, (inhomogeneousCochains M).d q (q + 1) (ih i) ∈
+        subrepToSubmodule (M_ n) (q + 1)) :
+      ∃ next : (inhomogeneousCochains M).X q,
+        x - (inhomogeneousCochains M).d q (q + 1) next -
+          ∑ i, (inhomogeneousCochains M).d q (q + 1) (ih i) ∈
+            subrepToSubmodule (M_ (n + 1)) (q + 1) := by
+    obtain ⟨y, hy⟩ := h₁ n <| π _ _ <| cocyclesMk (subrepToSubmoduleIso _ _ ⟨_, h⟩) <| by
+      ext f
+      rw [d_commutes]
+      simp only [CochainComplex.of_x, CochainComplex.of_d, map_sub, hx, map_sum, Pi.sub_apply,
+        Finset.sum_apply]
+      conv => enter [1,2,2]; exact funext fun c ↦ inhomogeneousCochains.d_d_apply _ _
+    induction y using groupCohomology_induction_on with | h y =>
+    sorry
   sorry
 
 end groupCohomology
