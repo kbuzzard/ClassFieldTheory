69135b57a1c02a1c9d73947268487ec2b1323dd0
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-11-09 00:52:28 +0000

    fix res

--- ClassFieldTheory/Cohomology/Functors/Inflation.lean
+++ ClassFieldTheory/Cohomology/Functors/Inflation.lean
@@ -42,71 +42,44 @@ variable {R G : Type} [CommRing R] [Group G] --[DecidableEq G]
 variable {Q : Type} [Group Q] {φ : G →* Q} (surj : Function.Surjective φ) -- [DecidableEq H]
 
 namespace Rep
-@[simps] noncomputable def quotientToInvariantsFunctor' :
-    Rep R G ⥤ Rep R Q where
-  obj M := M.quotientToInvariants φ.ker ↓ (QuotientGroup.quotientKerEquivOfSurjective φ surj).symm
-  map f := ofHom {
-    val := LinearMap.restrict (ModuleCat.Hom.hom f.hom) (by
-      intro x hx g
-      specialize hx g
-      simp only [MonoidHom.coe_comp, Subgroup.coe_subtype, Function.comp_apply] at hx ⊢
-      rw [←Rep.ρ_hom, ←LinearMap.comp_apply, ←ModuleCat.hom_comp, ←f.comm,
-        ModuleCat.hom_comp, LinearMap.comp_apply, Rep.ρ_hom, hx])
-    property h := by
-      rename_i X Y
-      ext ⟨x,hx⟩
-      rw [Function.comp_apply, Function.comp_apply]
-      apply Subtype.ext
-      change (ModuleCat.Hom.hom f.hom  ∘ₗ X.ρ _) _ = ((Y.ρ _) ∘ₗ (ModuleCat.Hom.hom f.hom)) _
-      rw [←Rep.ρ_hom, ←Rep.ρ_hom, ←ModuleCat.hom_comp, ←ModuleCat.hom_comp, f.comm]
-  }
-  map_id _ := rfl
-  map_comp _ _ := rfl
+
+@[simps! (isSimp := false) obj_V] noncomputable def quotientToInvariantsFunctor' :
+    Rep R G ⥤ Rep R Q :=
+  quotientToInvariantsFunctor R φ.ker ⋙
+    (Rep.resEquiv R (QuotientGroup.quotientKerEquivOfSurjective _ surj)).inverse
+
+/--
+`M ↑ H` means the `H` invariants of `M`, as a representation of `G ⧸ H`.
+-/
+notation3 M " ↑ " surj => (Rep.quotientToInvariantsFunctor' surj).obj M
 
 lemma quotientToInvariantsFunctor'_obj_ρ (M : Rep R G) :
-    ((quotientToInvariantsFunctor' surj).obj M).ρ =
-    (M.quotientToInvariants φ.ker).ρ.comp ((QuotientGroup.quotientKerEquivOfSurjective φ surj).symm)
-    := rfl
+    (M ↑ surj).ρ =
+    (M.quotientToInvariants φ.ker).ρ.comp
+      ((QuotientGroup.quotientKerEquivOfSurjective φ surj).symm) := rfl
+
+@[simp] lemma QuotientGroup.quotientKerEquivOfSurjective_symm_apply
+    {G H : Type*} [Group G] [Group H] (φ : G →* H) (hφ : (⇑φ).Surjective) (x : G) :
+    (QuotientGroup.quotientKerEquivOfSurjective φ hφ).symm (φ x) = x :=
+  MulEquiv.symm_apply_eq _ |>.mpr rfl
 
 lemma quotientToInvariantsFunctor'_obj_ρ_apply (M : Rep R G) (g : G) :
-    ((quotientToInvariantsFunctor' surj).obj M).ρ (φ g) =
-    (M.quotientToInvariants φ.ker).ρ g
-    := by
-  rw [quotientToInvariantsFunctor'_obj_ρ]
-  simp only [quotientToInvariantsFunctor'_obj, res, Action.res_obj_V, of_ρ, MonoidHom.coe_comp,
-    MonoidHom.coe_coe, Function.comp_apply]
-  congr
-  rw [MulEquiv.symm_apply_eq]
-  rfl
+    (M ↑ surj).ρ (φ g) = (M.quotientToInvariants φ.ker).ρ g := by
+  simp [quotientToInvariantsFunctor'_obj_V, quotientToInvariantsFunctor'_obj_ρ]
 
-lemma quotientToInvariantsFunctor'_obj_ρ_apply₂ (M : Rep R G) (g : G)
+@[simp] lemma quotientToInvariantsFunctor'_obj_ρ_apply₂ (M : Rep R G) (g : G)
     (v : (quotientToInvariantsFunctor' surj).obj M) :
-    (((quotientToInvariantsFunctor' surj).obj M).ρ (φ g) v).val =
-    M.ρ g v.val
-    := by
+    ((M ↑ surj).ρ (φ g) v).val = M.ρ g v.val := by
   rw [quotientToInvariantsFunctor'_obj_ρ_apply]
   rfl
 
 instance : (quotientToInvariantsFunctor' (R := R) surj).PreservesZeroMorphisms where
   map_zero _ _ := rfl
 
-set_option quotPrecheck false in
-/--
-`M ↑ H` means the `H` invariants of `M`, as a representation of `G ⧸ H`.
--/
-notation M " ↑ " surj => (Rep.quotientToInvariantsFunctor' surj).obj M
-
-noncomputable def res_quotientToInvariantsFunctor'_ι (M : Rep R G) :
+@[simps!] noncomputable def res_quotientToInvariantsFunctor'_ι (M : Rep R G) :
     ((M ↑ surj) ↓ φ) ⟶ M where
-  hom := ofHom (Submodule.subtype _)
-  comm g := by
-    ext m
-    simp only [res, quotientToInvariantsFunctor'_obj, Action.res_obj_V, Action.res_obj_ρ,
-      RingHom.toMonoidHom_eq_coe, RingEquiv.toRingHom_eq_coe, MonoidHom.coe_comp, MonoidHom.coe_coe,
-      RingHom.coe_coe, Function.comp_apply, ModuleCat.hom_comp, ModuleCat.hom_ofHom,
-      LinearMap.coe_comp, Submodule.coe_subtype, ρ_hom]
-    rw [←Rep.quotientToInvariantsFunctor'_obj_ρ_apply₂ surj M]
-    rfl
+  hom := ModuleCat.ofHom (Submodule.subtype _)
+  comm g := by ext; exact quotientToInvariantsFunctor'_obj_ρ_apply₂ surj ..
 
 end Rep
 namespace groupCohomology
