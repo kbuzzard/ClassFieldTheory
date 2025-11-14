a7b03de5e812667347b80a1a916b1f433d23a9fa
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-11-14 11:02:01 +0000

    tidy up

--- ClassFieldTheory/IsNonarchimedeanLocalField/Valuation.lean
+++ ClassFieldTheory/IsNonarchimedeanLocalField/Valuation.lean
@@ -8,11 +8,44 @@ import ClassFieldTheory.IsNonarchimedeanLocalField.Basic
 import Mathlib.FieldTheory.Galois.IsGaloisGroup
 /-
 
-# 1 → 𝒪[L]ˣ → Lˣ → ℤ → 0
+# 1 → 𝒪[K]ˣ → Kˣ → ℤ → 0
 
-We construct the short exact sequence `1 → 𝒪[L]ˣ → Lˣ → ℤ → 0` in Rep ℤ G.
+We construct the short exact sequence `0 → Additive (𝒪[K]ˣ) → Additive (Kˣ) → ℤ → 0` in
+the following sense: we define the maps `ker_v K` and `v K`, prove the first is
+injective, the second is surjective, and the pair is `Function.Exact`.
 
 -/
+
+namespace ValuativeRel
+
+section proofWanted
+
+-- I only need it for IsNonarchimedeanLocalField but it should be true in this generality
+lemma valuation_eq_one_iff (K : Type*) [Field K] [ValuativeRel K] (x : K) :
+  (ValuativeRel.valuation K) x = 1 ↔ ∃ r : 𝒪[K]ˣ, x = r := sorry
+
+-- Andrew Yang says he has a sorry-free proof of this:
+lemma valuation_ringEquiv {L : Type*} [Field L] [ValuativeRel L] [TopologicalSpace L]
+    [IsNonarchimedeanLocalField L] (e : L ≃+* L) (x : L) :
+    valuation L (e x) = valuation L x := sorry
+
+end proofWanted
+
+def ker_v (K : Type*) [CommRing K] [ValuativeRel K] : Additive (𝒪[K]ˣ) →+ Additive Kˣ :=
+  (Units.map (𝒪[K].subtype).toMonoidHom).toAdditive
+
+variable {K : Type*} [CommRing K] [ValuativeRel K]
+
+lemma ker_v_def (r : 𝒪[K]ˣ) :
+  ker_v K (.ofMul r) = .ofMul (Units.map (𝒪[K].subtype).toMonoidHom r) := rfl
+
+lemma ker_v_injective : (ker_v K : Additive (𝒪[K]ˣ) → Additive Kˣ).Injective := by
+  intro a b h
+  obtain ⟨a, rfl⟩ := Additive.ofMul.surjective a
+  obtain ⟨b, rfl⟩ := Additive.ofMul.surjective b
+  exact Units.map_injective ((Set.injective_codRestrict Subtype.property).mp fun ⦃a₁ a₂⦄ a ↦ a) h
+
+end ValuativeRel
 
 namespace IsNonarchimedeanLocalField
 
@@ -22,104 +55,65 @@ open ValuativeRel
 
 open scoped WithZero
 
+-- **TODO** Andrew also says: an IsGaloisGroup version of `Rep.ofAlgebraAutOnUnits`
+
 /--
 The ℤᵐ⁰-valued valuation on a nonarchimedean local field. Note that it sends
 a uniformiser to `.exp (-1)`.
 -/
 noncomputable def v₀ {K : Type*} [Field K] [ValuativeRel K] [TopologicalSpace K]
     [IsNonarchimedeanLocalField K] : K →*₀ ℤᵐ⁰ :=
-  let f₁ : K →*₀ ValueGroupWithZero K := ValuativeRel.valuation K
-  let f₂ : ValueGroupWithZero K →*₀ ℤᵐ⁰ := valueGroupWithZeroIsoInt K
-  f₂.comp f₁
-
-/--
-The valuation on the units of a nonarch local field. Taking values in
-`Multiplicative ℤ` so it can be a group homomorphism between multiplicative
-groups. Note that it sends a uniformiser to +1.
--/
-noncomputable def v {K : Type*} [Field K] [ValuativeRel K] [TopologicalSpace K]
-    [IsNonarchimedeanLocalField K] : Kˣ →* Multiplicative ℤ :=
-  let f₃ : Kˣ →* (ℤᵐ⁰)ˣ := Units.map (v₀.toMonoidHom : K →* ℤᵐ⁰)
-  -- here we introduce the sign
-  let f₄ : (ℤᵐ⁰)ˣ →* Multiplicative ℤ := (WithZero.unitsWithZeroEquiv.toMonoidHom)⁻¹
-  f₄.comp f₃
+  (valueGroupWithZeroIsoInt K : ValueGroupWithZero K →*₀ ℤᵐ⁰).comp
+  (ValuativeRel.valuation K : K →*₀ ValueGroupWithZero K)
 
 variable {K : Type*} [Field K] [ValuativeRel K] [TopologicalSpace K]
     [IsNonarchimedeanLocalField K]
-
-lemma v_def (x : Kˣ) : v x = v₀ (x⁻¹ : K) := by
-  simp [v, v₀]
 
 lemma v₀_uniformiser {ϖ : 𝒪[K]} (hϖ : Irreducible ϖ) :
     v₀ (ϖ : K) = .exp (-1) :=
   valueGroupWithZeroIsoInt_irreducible hϖ
 
+/--
+The valuation on the units of a nonarch local field. Domain is actually
+`Additive Kˣ` so that it can be an additive group homomorphism to ℤ.
+Normalised so that it sends a uniformiser to +1.
+-/
+noncomputable def v (K : Type*) [Field K] [ValuativeRel K] [TopologicalSpace K]
+    [IsNonarchimedeanLocalField K] : Additive Kˣ →+ ℤ :=
+  let f₃ : Kˣ →* (ℤᵐ⁰)ˣ := Units.map (v₀.toMonoidHom : K →* ℤᵐ⁰)
+  -- here we introduce the sign
+  let f₄ : (ℤᵐ⁰)ˣ →* Multiplicative ℤ := (WithZero.unitsWithZeroEquiv.toMonoidHom)⁻¹
+  (f₄.comp f₃).toAdditiveLeft
+
+lemma v_def (x : Kˣ) : Multiplicative.ofAdd (v K (.ofMul x)) = v₀ (x⁻¹ : K) := by
+  simp [v, v₀]
+
 lemma v_uniformiser {ϖ : 𝒪[K]} (hϖ : Irreducible ϖ) :
-    v (Units.mk0 (ϖ : K) hϖ.ne_zero') = .ofAdd 1 := by
+    v K (.ofMul <| Units.mk0 (ϖ : K) hϖ.ne_zero') = 1 := by
+  apply Multiplicative.ofAdd.injective
   apply WithZero.coe_injective
   simp [v_def, v₀_uniformiser hϖ, WithZero.exp]
 
-lemma v_surjective : Function.Surjective (v : Kˣ → Multiplicative ℤ) := by
+lemma v_surjective : (v K : Additive Kˣ → ℤ).Surjective := by
   intro n
   obtain ⟨ϖ₀, hϖ₀⟩ := IsDiscreteValuationRing.exists_irreducible (𝒪[K])
   let ϖ : Kˣ := Units.mk0 (ϖ₀ : K) hϖ₀.ne_zero'
-  use ϖ ^ n.toAdd
-  ext
+  use n • (.ofMul ϖ)
   simp [v_uniformiser hϖ₀, ϖ]
 
-lemma v_ker : v.ker = 𝒪[K].toSubmonoid.units := by
-  ext x
-  have hx : (x : K) ≠ 0 := x.ne_zero
-  rw [MonoidHom.mem_ker, ← WithZero.coe_inj, v_def]
-  simp only [map_inv₀, WithZero.coe_one, inv_eq_one]
-  simp [v₀]
-  /-
-  x : Kˣ
-  ⊢ (valuation K) ↑x = 1 ↔ x ∈ 𝒪[K].units
-  -/
-  sorry
+lemma v_eq_zero_iff (x : Kˣ) : v K (.ofMul x) = 0 ↔ valuation K x = 1 := by
+  rw [← Multiplicative.ofAdd.apply_eq_iff_eq, ← WithZero.coe_inj]
+  simp [v_def, v₀]
 
-noncomputable def vₐ : Additive Kˣ →+ ℤ := (v : Kˣ →* Multiplicative ℤ).toAdditiveLeft
+lemma ker_v_ker : Function.Exact (ker_v K) (v K) := by
+  intro x
+  obtain ⟨k, rfl⟩ := Additive.ofMul.surjective x
+  rw [v_eq_zero_iff, valuation_eq_one_iff]
+  simp only [Set.mem_range, Additive.exists, ker_v_def, Additive.ofMul.apply_eq_iff_eq]
+  apply exists_congr (fun r ↦ ?_)
+  rw [← Units.val_inj]
+  simp [Eq.comm]
 
 end valuation
 
-section short_exact_sequence
-
-variable (G K L : Type)
-    [Field K] [ValuativeRel K] [TopologicalSpace K] [IsNonarchimedeanLocalField K]
-    [Field L] [ValuativeRel L] [TopologicalSpace L] [IsNonarchimedeanLocalField L]
-    [Algebra K L] [ValuativeExtension K L]
-    [Group G] [Finite G] [MulSemiringAction G L] [IsGaloisGroup G K L]
-
-open CategoryTheory
-
-open scoped ValuativeRel
-
-noncomputable def valuationShortComplex : ShortComplex (Rep ℤ G) where
-  X₁ := (Rep.res <|
-          -- restrict along `G ≃* (𝒪[L] ≃ₐ[𝒪[K]] 𝒪[L]`
-          (IsGaloisGroup.mulEquivAlgEquiv G K L).trans (galRestrict 𝒪[K] K L 𝒪[L])).obj <|
-        -- Gal(L/K)-module Lˣ
-        Rep.ofAlgebraAutOnUnits 𝒪[K] 𝒪[L]
-        -- restrict along an isomorphism
-  X₂ := (Rep.res <|
-          -- G ≃* Gal(L/K)
-          IsGaloisGroup.mulEquivAlgEquiv G K L).obj <|
-        -- Gal(L/K)-module Lˣ
-        Rep.ofAlgebraAutOnUnits K L
-  X₃ := .trivial ℤ G ℤ
-  f := {
-    hom := ModuleCat.ofHom (Units.map 𝒪[L].subtype : 𝒪[L]ˣ →* Lˣ).toAdditive.toIntLinearMap
-    comm g := sorry -- should be easy (surprised it's not rfl)
-  }
-  g := {
-    hom := ModuleCat.ofHom (vₐ : (Additive Lˣ) →+ ℤ).toIntLinearMap
-    comm := sorry -- has some content, see https://leanprover.zulipchat.com/#narrow/channel/516717-Oxford-Class-Field-Theory-2025-workshop/topic/valuation.20exact.20sequence.20for.20herbrand.20quotient/near/554423613
-  }
-  zero := sorry -- v(𝒪[L]ˣ) = 0
-
--- first map monic should be easy, second map epi should be `IsNonarchimedeanLocalField.v_surjective`
--- and exactness in the middle should be v(x)=0 => x is a unit
-lemma valuationShortComplex.shortExact : (valuationShortComplex G K L).ShortExact := sorry
-
-end short_exact_sequence
+end IsNonarchimedeanLocalField
