16add88fdbe5fb3a2c2c6d5a36cd31915816a854
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-11-25 22:20:02 +0000

    statement of unramified cohomology

--- ClassFieldTheory/IsNonarchimedeanLocalField/Valuation.lean
+++ ClassFieldTheory/IsNonarchimedeanLocalField/Valuation.lean
@@ -5,40 +5,30 @@ Authors: Kevin Buzzard
 -/
 import ClassFieldTheory.Cohomology.Functors.Restriction
 import ClassFieldTheory.IsNonarchimedeanLocalField.Basic
+import ClassFieldTheory.Mathlib.Topology.Algebra.Valued.ValuativeRel
 import Mathlib.FieldTheory.Galois.IsGaloisGroup
 /-
 
 # 1 → 𝒪[K]ˣ → Kˣ → ℤ → 0
 
-We construct the short exact sequence `0 → Additive (𝒪[K]ˣ) → Additive (Kˣ) → ℤ → 0` in
-the following sense: we define the maps `ker_v K` and `v K`, prove the first is
+We construct the short exact sequence `0 → Additive 𝒪[K]ˣ → Additive (Kˣ) → ℤ → 0` in
+the following sense: we define the maps `kerV K` and `v K`, prove the first is
 injective, the second is surjective, and the pair is `Function.Exact`.
 
 -/
 
 namespace ValuativeRel
 
-section proofWanted
-
--- I only need it for IsNonarchimedeanLocalField but it should be true in this generality
-lemma valuation_eq_one_iff (K : Type*) [Field K] [ValuativeRel K] (x : K) :
-  (ValuativeRel.valuation K) x = 1 ↔ ∃ r : 𝒪[K]ˣ, x = r := sorry
-
-end proofWanted
-
-def ker_v (K : Type*) [CommRing K] [ValuativeRel K] : Additive (𝒪[K]ˣ) →+ Additive Kˣ :=
-  (Units.map (𝒪[K].subtype).toMonoidHom).toAdditive
+def kerV (K : Type*) [CommRing K] [ValuativeRel K] : Additive 𝒪[K]ˣ →+ Additive Kˣ :=
+  (Units.map 𝒪[K].subtype).toAdditive
 
 variable {K : Type*} [CommRing K] [ValuativeRel K]
 
-lemma ker_v_def (r : 𝒪[K]ˣ) :
-  ker_v K (.ofMul r) = .ofMul (Units.map (𝒪[K].subtype).toMonoidHom r) := rfl
+@[simp] lemma kerV_apply (r : 𝒪[K]ˣ) :
+    kerV K (.ofMul r) = .ofMul (Units.map (𝒪[K].subtype).toMonoidHom r) := rfl
 
-lemma ker_v_injective : (ker_v K : Additive (𝒪[K]ˣ) → Additive Kˣ).Injective := by
-  intro a b h
-  obtain ⟨a, rfl⟩ := Additive.ofMul.surjective a
-  obtain ⟨b, rfl⟩ := Additive.ofMul.surjective b
-  exact Units.map_injective ((Set.injective_codRestrict Subtype.property).mp fun ⦃a₁ a₂⦄ a ↦ a) h
+lemma kerV_injective : (⇑(kerV K)).Injective :=
+  Units.map_injective Subtype.val_injective
 
 end ValuativeRel
 
@@ -51,19 +41,26 @@ open ValuativeRel
 open scoped WithZero
 
 /--
-The ℤᵐ⁰-valued valuation on a nonarchimedean local field. Note that it sends
+The `ℤᵐ⁰`-valued valuation on a nonarchimedean local field. Note that it sends
 a uniformiser to `.exp (-1)`.
 -/
-noncomputable def v₀ {K : Type*} [Field K] [ValuativeRel K] [TopologicalSpace K]
-    [IsNonarchimedeanLocalField K] : K →*₀ ℤᵐ⁰ :=
-  (valueGroupWithZeroIsoInt K : ValueGroupWithZero K →*₀ ℤᵐ⁰).comp
-  (ValuativeRel.valuation K : K →*₀ ValueGroupWithZero K)
+noncomputable def valuationInt (K : Type*) [Field K] [ValuativeRel K] [TopologicalSpace K]
+    [IsNonarchimedeanLocalField K] : Valuation K ℤᵐ⁰ :=
+  (valuation K).map (valueGroupWithZeroIsoInt K) <| OrderHomClass.mono <| valueGroupWithZeroIsoInt K
 
 variable {K : Type*} [Field K] [ValuativeRel K] [TopologicalSpace K]
-    [IsNonarchimedeanLocalField K]
+  [IsNonarchimedeanLocalField K]
 
-lemma v₀_uniformiser {ϖ : 𝒪[K]} (hϖ : Irreducible ϖ) :
-    v₀ (ϖ : K) = .exp (-1) :=
+-- move
+theorem _root_.OrderIsoClass.strict_mono {α β F : Type*} [Preorder α] [Preorder β]
+    [EquivLike F α β] [OrderIsoClass F α β] (f : F) : StrictMono f := fun x y ↦
+  by simp [lt_iff_le_not_ge]
+
+instance : (valuationInt K).Compatible :=
+  Valuation.compatible_map _ <| OrderIsoClass.strict_mono _
+
+lemma valuationInt_uniformiser {ϖ : 𝒪[K]} (hϖ : Irreducible ϖ) :
+    valuationInt K ϖ = .exp (-1) :=
   valueGroupWithZeroIsoInt_irreducible hϖ
 
 /--
@@ -73,39 +70,44 @@ Normalised so that it sends a uniformiser to +1.
 -/
 noncomputable def v (K : Type*) [Field K] [ValuativeRel K] [TopologicalSpace K]
     [IsNonarchimedeanLocalField K] : Additive Kˣ →+ ℤ :=
-  let f₃ : Kˣ →* (ℤᵐ⁰)ˣ := Units.map (v₀.toMonoidHom : K →* ℤᵐ⁰)
+  let f₃ : Kˣ →* (ℤᵐ⁰)ˣ := Units.map (valuationInt K)
   -- here we introduce the sign
   let f₄ : (ℤᵐ⁰)ˣ →* Multiplicative ℤ := (WithZero.unitsWithZeroEquiv.toMonoidHom)⁻¹
   (f₄.comp f₃).toAdditiveLeft
 
-lemma v_def (x : Kˣ) : Multiplicative.ofAdd (v K (.ofMul x)) = v₀ (x⁻¹ : K) := by
-  simp [v, v₀]
+@[simp] lemma v_apply (x : Additive Kˣ) : v K x = -(valuationInt K x.toMul).log := by
+  obtain ⟨x, rfl⟩ := Additive.ofMul.surjective x
+  rw [toMul_ofMul]
+  generalize hy : valuationInt K x = y
+  cases y using WithZero.expRecOn with
+  | zero => simp at hy
+  | exp y => simp [v, hy]
 
 lemma v_uniformiser {ϖ : 𝒪[K]} (hϖ : Irreducible ϖ) :
     v K (.ofMul <| Units.mk0 (ϖ : K) hϖ.ne_zero') = 1 := by
-  apply Multiplicative.ofAdd.injective
-  apply WithZero.coe_injective
-  simp [v_def, v₀_uniformiser hϖ, WithZero.exp]
+  simp [valuationInt_uniformiser hϖ]
 
 lemma v_surjective : (v K : Additive Kˣ → ℤ).Surjective := by
   intro n
-  obtain ⟨ϖ₀, hϖ₀⟩ := IsDiscreteValuationRing.exists_irreducible (𝒪[K])
-  let ϖ : Kˣ := Units.mk0 (ϖ₀ : K) hϖ₀.ne_zero'
-  use n • (.ofMul ϖ)
-  simp [v_uniformiser hϖ₀, ϖ]
+  obtain ⟨ϖ₀, hϖ₀⟩ := IsDiscreteValuationRing.exists_irreducible 𝒪[K]
+  use n • .ofMul (.mk0 (ϖ₀ : K) hϖ₀.ne_zero')
+  simp [valuationInt_uniformiser hϖ₀]
+
+-- move
+@[simp] lemma _root_.WithZero.log_eq_zero_iff {α : Type*} [AddMonoid α] {x : αᵐ⁰} :
+    x.log = 0 ↔ x = 0 ∨ x = 1 := by
+  cases x using WithZero.expRecOn with
+  | zero => simp
+  | exp x => simp
 
 lemma v_eq_zero_iff (x : Kˣ) : v K (.ofMul x) = 0 ↔ valuation K x = 1 := by
-  rw [← Multiplicative.ofAdd.apply_eq_iff_eq, ← WithZero.coe_inj]
-  simp [v_def, v₀]
+  simpa using (ValuativeRel.isEquiv _ _).eq_one_iff_eq_one
 
-lemma ker_v_ker : Function.Exact (ker_v K) (v K) := by
+lemma exact_kerV_v : Function.Exact (kerV K) (v K) := by
   intro x
-  obtain ⟨k, rfl⟩ := Additive.ofMul.surjective x
+  obtain ⟨x, rfl⟩ := Additive.ofMul.surjective x
   rw [v_eq_zero_iff, valuation_eq_one_iff]
-  simp only [Set.mem_range, Additive.exists, ker_v_def, Additive.ofMul.apply_eq_iff_eq]
-  apply exists_congr (fun r ↦ ?_)
-  rw [← Units.val_inj]
-  simp [Eq.comm]
+  simp [Units.ext_iff]
 
 end valuation
 
