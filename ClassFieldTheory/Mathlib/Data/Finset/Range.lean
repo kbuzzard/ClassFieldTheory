ee1468ff36dfa98b3c22017d4eee27a7ee78979b
Yaël Dillies <yael.dillies@gmail.com>
2026-03-20 14:39:27 +0000

    Bump mathlib to v4.28.0 (#131)

--- ClassFieldTheory/Mathlib/Data/Finset/Range.lean
+++
@@ -1,8 +0,0 @@
-module
-
-public import Mathlib.Data.Finset.Range
-
-public section
-
-theorem Multiset.toFinset_range {n : ℕ} : (Multiset.range n).toFinset = .range n :=
-  Finset.val_injective (Finset.range n).nodup.dedup
